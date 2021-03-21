3a611536acb4838f5e23cbcedb9bdf9450954fc0
yanina.husak <yanina.husak@leverx.com>
2021-03-21 22:26:34 +0300

    run script: logging

--- catarl/run.sh
+++ catarl/run.sh
@@ -1,3 +1,3 @@
-redis-server --port 12000 &
-CUDA_VISIBLE_DEVICES="0" catalyst-rl run-trainer --config=configs/basic.yml &
-CUDA_VISIBLE_DEVICES="" catalyst-rl run-samplers --config=configs/basic.yml &
+redis-server --port 12000  >> "redis_logs.txt" 2>> "redis_logs.txt" &
+CUDA_VISIBLE_DEVICES="0" catalyst-rl run-trainer --config=configs/basic.yml  >> "trainer_logs.txt"  2>> "trainer_logs.txt" &
+CUDA_VISIBLE_DEVICES="" catalyst-rl run-samplers --config=configs/basic.yml  >> "samplers_logs.txt" 2>> "samplers_logs.txt" &
